29f54f16a1d38374736ee0ff87625a07c76d4ac9
itayg43 <93944494+itayg43@users.noreply.github.com>
2024-06-03 18:05:49 +0300

    edit: create test for user list

---
+++ tests/components/UserList.test.tsx
@@ -0,0 +1,26 @@
+import { User } from "../../src/entities";
+import UserList from "../../src/components/UserList";
+import { render, screen } from "@testing-library/react";
+
+describe("UserList", () => {
+  it("should render no users when the users array is empty", () => {
+    render(<UserList users={[]} />);
+
+    expect(screen.getByText(/no users/i)).toBeInTheDocument();
+  });
+
+  it("should render a list of users", () => {
+    const users: User[] = [
+      { id: 1, name: "Itay" },
+      { id: 2, name: "Amit" },
+    ];
+
+    render(<UserList users={users} />);
+
+    users.forEach((user) => {
+      const link = screen.getByRole("link", { name: user.name });
+      expect(link).toBeInTheDocument();
+      expect(link).toHaveAttribute("href", `/users/${user.id}`);
+    });
+  });
+});
